3375342987d23a3bf84616d97f1de4790e939f66
samueltiokeng <samueledward40@gmail.com>
2020-09-25 07:35:56 +0200

    joke

---
+++ dadjokes/components/joke.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="joke">
+        <p>{{ joke }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Joke',
+    props: ['joke']
+}
+</script>
+
+<style>
+
+</style>
